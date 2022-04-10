69e3c2d6e2ef91b549eea9c29f8ae339ca9ccc64
bbashcode <binbash.code@gmail.com>
2022-04-10 08:52:43 +0000

    install body-parser npm package and include it in the server file

--- express_server.js
+++ express_server.js
@@ -1,8 +1,13 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const PORT = 8080; // default port 8080
 
+
+//middlewares
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
