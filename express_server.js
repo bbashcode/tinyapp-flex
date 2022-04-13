62bc66210b814409d62ea77d6bd655b34625734a
bbashcode <binbash.code@gmail.com>
2022-04-13 20:52:34 +0000

    implement login route and logged in as username using cookies

--- express_server.js
+++ express_server.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+
 const app = express();
-const PORT = 8080; // default port 8080
-
-
+const PORT = 8080;
 //middlewares
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
 
 // TODO: possibly move to data layer
 let urlDatabase = {
@@ -29,18 +30,19 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = {urls: urlDatabase};
+  const templateVars = {urls: urlDatabase, username: req.cookies["username"]};
   res.render("urls_index", templateVars);
 });
 
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = {username: req.cookies["username"]};
+  res.render("urls_new", templateVars);
 });
 
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = {shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
+  const templateVars = {shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], username: req.cookies["username"]};
   res.render("urls_show", templateVars);
 });
 
@@ -71,8 +73,10 @@ app.post("/urls/:shortURL", (req, res)=> {
 })
 
 
-app.post("", () => {
-
+app.post("/login", (req, res) => {
+  res.cookie("username", req.body.username);
+  // console.log("username", req.body.userName);
+  res.redirect("/urls");
 });
 
 
